cb8f87b64c3b3364cdc2b5bdfe3612ba91cfbfc3
Giorgi Isaiashvili <giorgi.isaiashvili@gmail.com>
2020-04-04 13:55:10 +0400

    1. moved comments about navigation component from CategoriesScreen to MealsNavigator.
    2. added dummy-data and category model

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -18,3 +18,23 @@ const MealsNavigator = () => {
 }
 
 export default MealsNavigator;
+
+
+//   Here is some useful information about navigation methods.
+
+//   1. navigate will not always navigate you to another screen
+//      e.x. if you are already in that scree, but push will always.
+//      onPress={() => { props.navigation.push('CategoryMeals') }}
+//   2. goBack() and pop(). Navigation has also functions goBack and pop, back will 
+//      get you in previous scree, and pop will pop current screem
+//      that means you will get in previous screen. Difference is that
+//      pop can only be used in stack navigator, goBack works in every navigation      
+//   3. popToTop() takes you to the root screen
+//   4. replace() replaces current screen with another, that means stack is empty and
+//      goBack can't be used. e.x. This will be usefull when user logs in
+
+// Test button that was implemented in CategoriesScreen
+
+// <Button
+//title='Go to CategoryMeals'
+//onPress={() => { props.navigation.navigate('CategoryMeals') }}/>
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -7,7 +7,18 @@ const CategoriesScreen = props => {
             <Text>The CategoriesScreen</Text>
             <Button
                 title='Go to CategoryMeals'
-                onPress={() => { props.navigation.navigate('CategoryMeals') }} />
+                // 1. navigate will not always navigate you to another screen
+                //    e.x. if you are already in that scree, but push will always.
+                //    onPress={() => { props.navigation.push('CategoryMeals') }}
+                // 2. goBack() and pop(). Navigation has also functions goBack and pop, back will 
+                //    get you in previous scree, and pop will pop current screem
+                //    that means you will get in previous screen. Difference is that
+                //    pop can only be used in stack navigator, goBack works in every navigation      
+                // 3. popToTop() takes you to the root screen
+                // 4. replace() replaces current screen with another, that means stack is empty and
+                //    goBack can't be used. e.x. This will be usefull when user logs in
+                onPress={() => { props.navigation.navigate('CategoryMeals') }}
+                 />
         </View>
     );
 }
