6cae9497493e2c644530ac4b214f7c4e3c91d455
Giorgi Isaiashvili <giorgi.isaiashvili@gmail.com>
2020-04-18 22:55:38 +0400

    added filter meal action

--- store/reducers/mealsReducer.js
+++ store/reducers/mealsReducer.js
@@ -1,5 +1,5 @@
 import { MEALS } from "../../data/dummy-data";
-import { TOGGLE_FAVORITE } from "../actions/mealsAction";
+import { TOGGLE_FAVORITE, SET_FILTERS } from "../actions/mealsAction";
 
 const initialState = {
     allMeals: MEALS,
@@ -8,10 +8,9 @@ const initialState = {
 }
 
 const mealsReducer = (state = initialState, action) => {
-
     switch (action.type) {
         case TOGGLE_FAVORITE:
-            const existingIndex = state.favoriteMeals.findIndex(meal =>meal.id === action.mealId);
+            const existingIndex = state.favoriteMeals.findIndex(meal => meal.id === action.mealId);
             console.log(existingIndex);
             if (existingIndex >= 0) {
                 const updatedFavMeals = [...state.favoriteMeals];
@@ -28,7 +27,24 @@ const mealsReducer = (state = initialState, action) => {
                     favoriteMeals: state.favoriteMeals.concat(mealToAdd)
                 };
             }
-
+        case SET_FILTERS:
+            const appliedFilters = action.filters;
+            const updatedFilteredMeals = state.meals.filter(meal => {
+                if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+                    return false;
+                }
+                if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+                    return false;
+                }
+                if (appliedFilters.vegetarian && !meal.isVegetarian) {
+                    return false;
+                }
+                if (appliedFilters.vegan && !meal.isVegan) {
+                    return false;
+                }
+                return true;
+            });
+            return { ...state, filteredMeals: updatedFilteredMeals };
         default:
             return state;
     };
